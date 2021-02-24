5e240d206455f37775e879d97c5d46ebbc19e22d
MNurRizqiSaputra <putra15x@gmail.com>
2021-02-24 15:17:13 +0700

    belajar php dasar

---
+++ dasar.php
@@ -0,0 +1,10 @@
+<?php
+
+    $angka = 2;
+    $angka2 = 9;
+    $angka3 = $angka2 + $angka;
+    echo $angka + $angka2 ;
+    echo "</br>";
+    echo "jadi angka sama dengan = $angka3";
+    echo "dokumen saya :" .$_server["DOCUMENT_ROOT"];
+?>
